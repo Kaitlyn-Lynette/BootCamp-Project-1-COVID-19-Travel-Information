67fe19abed5e1f24d43703324913c04a669fa49b
Peter Lam <Piitaa1234@gmail.com>
2020-04-15 13:23:59 -0400

    Updated script files

--- script.js
+++ script.js
@@ -1,26 +1,30 @@
-// queryURL
+// QueryURL for covid-19 Data
 var queryURL = "https://api.covid19api.com/summary";
 $.ajax({
   url: queryURL,
   method: "GET",
-}).then(function (response) {
-  console.log(response);
+}).then(function (responsecovid) {
+  console.log(responsecovid);
 
+  // QueryURL for NYTimes World News Data
   var queryURL =
     "https://api.nytimes.com/svc/topstories/v2/world.json?api-key=L4hQpiyOGEibjPv6zTL3iHLGCrbGgIER";
   $.ajax({
     url: queryURL,
     method: "GET",
-  }).then(function (response) {
-    console.log(response);
+  }).then(function (responsenyt) {
+    console.log(responsenyt);
+
+    // Starting Data
+    var totalConfirmed = responsecovid.Global.TotalConfirmed;
+    var totalDeaths = responsecovid.Global.TotalDeaths;
+    var totalRecovered = responsecovid.Global.TotalRecovered;
+
+    // Storing data from QueryURL
+
+    // Displaying infomration on screen
   });
 });
-// Starting Data
-
-// Storing data from QueryURL
-console.log(array.countries[0]);
-
-// Displaying infomration on screen
 
 // Search button
 $("#search").on("click", function (event) {});
