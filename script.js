6e2df3c17f69364a9d08ba86189bd3ccd116cda4
Kaitlyn Rodriguez <kaitlyn.l.rodriguez@gmail.com>
2020-04-16 11:08:23 -0400

    Kaitlyn's edits

--- script.js
+++ script.js
@@ -1,37 +1,40 @@
-var queryURL = "https://api.covid19api.com/summary";
-$.ajax({
-  url: queryURL,
-  method: "GET",
-}).then(function (responsecovid) {
-  console.log(responsecovid);
+$("#search").on("click", function (event) {
+  event.preventDefault();
+  //This is pulling the value of the country from the search bar
+  var country = $("#location-search").val();
+  console.log(country);
+  //This is an object where the keys are should come from a drop down and the values are the isoCodes
+  var iSOCodes = { USA: "us", Germany: "de" };
+  const keys = Object.keys(iSOCodes);
+  console.log(keys);
+  console.log(iSOCodes[keys[0]]);
 
-  // Make a variable for the country and get this value from the search
-  var queryURL =
-    "https://newsapi.org/v2/top-headlines?country=us&apiKey=d7814b6763714644a52a112b30add51b";
-
+  //We will query this API for stats on COVID
+  var queryURL = "https://api.covid19api.com/summary";
   $.ajax({
     url: queryURL,
     method: "GET",
-  }).then(function (responsenews) {
-    // Starting Data
+  }).then(function (responsecovid) {
+    console.log(responsecovid);
     var totalConfirmed = responsecovid.Global.TotalConfirmed;
     console.log(totalConfirmed);
     var totalDeaths = responsecovid.Global.TotalDeaths;
     console.log(totalDeaths);
     var totalRecovered = responsecovid.Global.TotalRecovered;
     console.log(totalRecovered);
+    // We will query this API for top headlines
+    var queryURL =
+      "https://newsapi.org/v2/top-headlines?country=" +
+      country +
+      "&apiKey=d7814b6763714644a52a112b30add51b";
+    console.log(queryURL);
+    $.ajax({
+      url: queryURL,
+      method: "GET",
+    }).then(function (responsenews) {
+      console.log(responsenews);
+    });
   });
-  // Storing data from QueryURL
-
-  // Displaying infomration on screen for news.
-  console.log(responsenews);
-});
-
-// Search button
-$("#search").on("click", function (event) {
-  event.preventDefault();
-  var country = $("#location-search").val();
-  console.log(country);
 });
 
 // Clear Button
